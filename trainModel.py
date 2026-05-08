2bc63b0e05429264f6b222e6d62d6bf9b863fbf6
Kate (PhD) <katephd@Kates-MacBook-Pro.local>
2026-05-08 10:18:17 +0200

    get train IDs returns only ids of images which have a pleural effusion label of 0 or 1

--- trainModel.py
+++ trainModel.py
@@ -5,10 +5,11 @@ import os
 from torch.utils.data import Subset
 import pickle as pkl
 import numpy as np
-from resnetTrainUtils import runTraining
+#from resnetTrainUtils import runTraining
 import torchvision.transforms as transforms
 import random
 from cheXpertDataset import CheXpertDataset
+import pandas as pd
 
 
 # set up the argument parser
@@ -33,10 +34,22 @@ batch_size = 5
 image_size = 128
 
 
-def getTrainIDs(data, n_samples=1000):
+def getTrainIDs(n_samples=1000):
     # get the IDs of the training data from the CheXpert dataset
-    ids = random.sample(range(0, len(data)), 1000)
 
+    # First return the IDs of the images that have a pleural effusion label of 1 OR 0, then return a random sample of the remaining IDs to make up a total of n_samples
+    # ignore -1 or NaN labels for pleural effusion
+    df = pd.read_csv(os.path.join(data_dir, "CheXpertSmall", "train_reduced.csv"))
+    pleural_effusion_ids = df[(df['Pleural Effusion'] == 1.0) | (df['Pleural Effusion'] == 0.0)]['image_id'].values
+
+    # select a random sample of the pleural effusion ids to make up a total of n_samples
+    if len(pleural_effusion_ids) >= n_samples:
+        ids = np.array(random.sample(list(pleural_effusion_ids), n_samples))
+    else:
+        # throw a warning if there are not enough pleural effusion ids to make up n_samples
+        print(f"Warning: there are only {len(pleural_effusion_ids)} pleural effusion ids in the dataset, which is less than the requested n_samples of {n_samples}. Returning all pleural effusion ids.")
+        ids = pleural_effusion_ids
+    
     return ids
 
 def getValidationIDs(data, n_samples=1000):
@@ -62,15 +75,16 @@ def main():
     train_data = Subset(dataset, ids)
 
     # select a subset of the data to validate the ResNet classifier on
-    validation_ids = getValidationIDs()
+    validation_ids = getValidationIDs(dataset)
     validation_data = Subset(dataset, validation_ids)
 
     # select a subset of the data to test the ResNet classifier on
-    test_ids = getValidationIDs()
+    test_ids = getValidationIDs(dataset)
     test_data = Subset(dataset, test_ids)
 
 
     # train the ResNet classifier on the selected subset of data and log results in MLFlow
+    """
     metrics = runTraining(train_data,
                           validation_data,
                           test_data,
@@ -81,6 +95,7 @@ def main():
                           image_size,
                           'resnet50',
                           True)
+    """
 
 
 
